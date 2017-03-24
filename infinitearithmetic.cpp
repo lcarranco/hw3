9b76a59b472b53cdc3af09a0ec362763d9871db0
lcarranco <lcarranco@gmail.com>
2017-03-24 13:43:51 -0500

    Format changes

--- infinitearithmetic.cpp
+++ infinitearithmetic.cpp
@@ -13,28 +13,23 @@
 
 using namespace std;
 
-struct Node
-{
+struct Node {
     long long num = 0;
     string str;
     Node *prev = 0;
     Node *next = 0;
 };
-class DoubleLinkedList
-{
+class DoubleLinkedList {
   public:
     DoubleLinkedList() {}
     // Destructor
-    ~DoubleLinkedList()
-    {
+    ~DoubleLinkedList() {
         //cout << "DLL ~" << endl;
         clear();
     }
 
-    void clear()
-    {
-        while (head != nullptr)
-        {
+    void clear() {
+        while (head != nullptr) {
             Node *deleteme = head;
             head = head->next;
             delete deleteme;
@@ -42,99 +37,83 @@ class DoubleLinkedList
     }
 
     // Copy constructor
-    DoubleLinkedList(DoubleLinkedList const &list)
-    {
+    DoubleLinkedList(DoubleLinkedList const &list) {
         //cout << "DLL copy" << endl;
     }
 
-    DoubleLinkedList &operator=(DoubleLinkedList const &other)
-    {
+    DoubleLinkedList &operator=(DoubleLinkedList const &other) {
         //cout << "DLL =" << endl;
         clear();
 
         Node *temp = other.head;
-        while (temp != nullptr)
-        {
+        while (temp != nullptr) {
             push_back(temp->str);
             temp = temp->next;
         }
         return *this;
     }
 
-    void push_front(string parted)
-    {
+    void push_front(string parted) {
         Node *temp = new Node();
         temp->str = parted;
         stringstream ss(parted);
         ss >> temp->num;
-        if (head == 0)
-        {
+        if (head == 0) {
             head = temp;
             tail = temp;
         }
-        else
-        {
+        else {
             head->prev = temp;
             temp->next = head;
             head = temp;
         }
     }
 
-    void push_back(string parted)
-    {
+    void push_back(string parted) {
         Node *temp = new Node();
         temp->str = parted;
         stringstream ss(parted);
         ss >> temp->num;
-        if (head == 0)
-        {
+        if (head == 0) {
             head = temp;
             tail = temp;
         }
-        else
-        {
+        else {
             tail->next = temp;
             temp->prev = tail;
             tail = temp;
         }
     }
 
-    void print(ostream &out) const
-    {
+    void print(ostream &out) const {
         Node *temp = head;
-        while (temp != nullptr)
-        {
+        while (temp != nullptr) {
             out << temp->str;
             temp = temp->next;
         }
     }
 
-    Node at(int index) const
-    {
+    Node at(int index) const {
         int i = 0;
         Node *temp = head;
-        while (i != index)
-        {
+        while (i != index) {
             temp = temp->next;
             i++;
         }
         return *temp;
     }
 
-    int size() const
-    {
+    int size() const {
         int count = 0;
         Node *temp = head;
-        while (temp != 0)
-        {
+        while (temp != 0) {
             temp = temp->next;
             count++;
         }
         return count;
     }
 
-    void swap(DoubleLinkedList &other)
-    {
+    void swap(DoubleLinkedList &other) {
         Node *temp = head;
         head = other.head;
         other.head = temp;
@@ -147,9 +126,7 @@ class DoubleLinkedList
 
 template <class Type>
 
-class stackType
-
-{
+class stackType {
   private:
     int maxStackSize;
     int stackTop;
@@ -168,23 +145,20 @@ class stackType
 
 template <class Type>
 
-void stackType<Type>::initializeStack()
-{
+void stackType<Type>::initializeStack() {
     stackTop = 0;
 } //   end function initializeStack
 
 template <class Type>
 
-bool stackType<Type>::isFullStack() const
-{
+bool stackType<Type>::isFullStack() const {
     return (stackTop == maxStackSize);
 } //   end function isFullStack
 
 //     check for stack empty
 template <class Type>
 
-bool stackType<Type>::isEmptyStack() const
-{
+bool stackType<Type>::isEmptyStack() const {
     return (stackTop == 0);
 } //   end function isEmptyStack
 
@@ -192,10 +166,8 @@ bool stackType<Type>::isEmptyStack() const
 
 template <class Type>
 
-void stackType<Type>::push(const Type &newItem)
-{
-    if (!isFullStack())
-    {
+void stackType<Type>::push(const Type &newItem) {
+    if (!isFullStack()) {
         list[stackTop] = newItem;
         stackTop++;
     }
@@ -207,8 +179,7 @@ void stackType<Type>::push(const Type &newItem)
 
 template <class Type>
 
-void stackType<Type>::pop()
-{
+void stackType<Type>::pop() {
     if (!isEmptyStack())
         stackTop--;
     else
@@ -219,9 +190,8 @@ void stackType<Type>::pop()
 
 template <class Type>
 
-Type stackType<Type>::top() const
-{
-	if (stackTop == 0){
+Type stackType<Type>::top() const {
+	if (stackTop == 0) {
 		cout << "here" << endl;
 	}
 		assert(stackTop != 0);
@@ -233,16 +203,13 @@ Type stackType<Type>::top() const
 
 template <class Type>
 
-stackType<Type>::stackType(int stackSize)
-{
-    if (stackSize <= 0)
-    {
+stackType<Type>::stackType(int stackSize) {
+    if (stackSize <= 0) {
         cout << "Invalid size";
         stackSize = 10;
     } //     end if
     else
         maxStackSize = stackSize;
-
     stackTop = 0;
     list = new Type[maxStackSize];
 
@@ -252,52 +219,43 @@ stackType<Type>::stackType(int stackSize)
 
 template <class Type>
 
-stackType<Type>::~stackType()
-{
+stackType<Type>::~stackType() {
     delete[] list;
 } //   end destructor stackType
 
-class Bigex
-{
+class Bigex {
 public:
     Bigex() {}
-    ~Bigex()
-    {
+    ~Bigex() {
         //cout << "BN ~" << endl;
     }
 
     Bigex(string expression, int digitsPerNode)
-        : digitsPerNode(digitsPerNode)
-    {
-        if(expression[0] == '-')
-        {
+        : digitsPerNode(digitsPerNode) {
+        if(expression[0] == '-') {
             isNegative = true;
             expression.erase(expression.begin());
         }
         int startIndex = expression.size() - digitsPerNode;
         // BUG: an extra empty node is being added
         // FIX: swap the = for next two statements
-        while(startIndex > 0)
-        {
+        while(startIndex > 0) {
             //cout << "exp " << expression.substr(startIndex, digitsPerNode) << endl;
             data.push_front(expression.substr(startIndex, digitsPerNode));
             startIndex -= digitsPerNode;
         }
-        if(startIndex <= 0)
-        {
+        if(startIndex <= 0) {
             //cout << "exp " << expression.substr(0, startIndex + digitsPerNode) << endl;
             data.push_front(expression.substr(0, startIndex + digitsPerNode));
         }
     }
-    Bigex(Bigex const &other)
-    {
+    Bigex(Bigex const &other) {
         //cout << "BN copy" << endl;
         isNegative = other.isNegative;
         data = other.data;
         digitsPerNode = other.digitsPerNode;
     }
-    Bigex &operator=(Bigex const &other)
-    {
+    Bigex &operator=(Bigex const &other) {
         //cout << "BN =" << endl;
         isNegative = other.isNegative;
         data = other.data;
@@ -305,21 +263,17 @@ public:
         return *this;
     }
 
-    Bigex operator+(Bigex const &other)
-    {
+    Bigex operator+(Bigex const &other) {
         Bigex const & bigexA = *this;
         Bigex const & bigexB = other;
         Bigex result;
-        if(!bigexA.isNegative && bigexB.isNegative)
-        {
+        if(!bigexA.isNegative && bigexB.isNegative) {
             // return bigexA - bigexB;
         }
-        if(bigexA.isNegative && !bigexB.isNegative)
-        {
+        if(bigexA.isNegative && !bigexB.isNegative) {
             // return bigexB - bigexA;
         }
-        if(bigexA.isNegative && bigexB.isNegative)
-        {
+        if(bigexA.isNegative && bigexB.isNegative) {
             result.isNegative = true;
         }
         int rollover = 0;
@@ -327,18 +281,15 @@ public:
         int indexB = bigexB.data.size() - 1;
 
         // add all nodes together
-        while(indexA > -1 || indexB > -1)
-        {
+        while(indexA > -1 || indexB > -1) {
             int a = 0;
             int b = 0;
 
-            if(indexA > -1)
-            {
+            if(indexA > -1) {
                 a = bigexA.data.at(indexA).num;
                 indexA--;
             }
-            if(indexB > -1)
-            {
+            if(indexB > -1) {
                 b = bigexB.data.at(indexB).num;
                 indexB--;
             }
@@ -352,15 +303,13 @@ public:
             ss << num;
             ss >> str;
             // this says if we aren't on the last node, pad the string
-            if(rollover > 0 || indexA > -1 || indexB > -1)
-            {
+            if(rollover > 0 || indexA > -1 || indexB > -1) {
                 padzeros(str);
             }
             result.data.push_front(str);
         }
         // add a node for the carry if necessary
-        if(rollover > 0)
-        {
+        if(rollover > 0) {
             string str;
             stringstream ss;
             ss << rollover;
@@ -368,8 +317,7 @@ public:
             result.data.push_front(str);
         }
 
-        //while(indexA >= 0 && indexB >= 0)
-        //{
+        //while(indexA >= 0 && indexB >= 0) {
         //    // the problem is, big numbers are usually created with a string
         //    // but in here, we create it based on adding two other nums
         //    // the nums of course don't have leading zeros, unlike strings
@@ -387,8 +335,7 @@ public:
         //    indexA--;
         //    indexB--;
         //}
-        //while(indexA >= 0)
-        //{
+        //while(indexA >= 0) {
         //    long long num = a.data.at(indexA).num + rollover;
         //    rollover = num / pow(10, digitsPerNode);
         //    num = num % (int)pow(10, digitsPerNode);
@@ -399,8 +346,7 @@ public:
         //    result.data.push_front(str);
         //    indexA--;
         //}
-        //while(indexB >= 0)
-        //{
+        //while(indexB >= 0) {
         //    long long num = b.data.at(indexB).num + rollover;
         //    rollover = num / pow(10, digitsPerNode);
         //    num = num % (int)pow(10, digitsPerNode);
@@ -411,28 +357,23 @@ public:
         //    result.data.push_front(str);
         //    indexB--;
         //}
-
         return result;
     }
 
-    void print(ostream &out) const
-    {
-        if(isNegative)
-        {
+    void print(ostream &out) const {
+        if(isNegative) {
             out << "-";
         }
         data.print(out);
     }
 
-    void swap(Bigex &other)
-    {
+    void swap(Bigex &other) {
         std::swap(isNegative, other.isNegative);
         data.swap(other.data);
     }
 
 private:
-    void padzeros(string & str)
-    {
+    void padzeros(string & str) {
         // create a new string with the proper amount of zeros
         string pad(digitsPerNode - str.size(), '0');
         str = pad + str;
@@ -442,8 +383,7 @@ private:
     int digitsPerNode;
 };
 
-class infixToPostfix
-{
+class infixToPostfix {
     string infix;
     string postfix;
     DoubleLinkedList data;
@@ -453,51 +393,41 @@ class infixToPostfix
     bool isOperator(const char c);
     int precedence(const char op1, const char op2);
     //void convert_sign();
-    void getInfix(string infixString)
-    {
+    void getInfix(string infixString) {
         infix = infixString;
     }
 
-    void showInfix()
-    {
+    void showInfix() {
         cout << "\n\tInfix expression: " << infix;
     }
 
-    void evaluatePostfix(int digitsPerNode)
-    {
+    void evaluatePostfix(int digitsPerNode) {
         string str;
         stringstream ss(postfix);
         stackType<Bigex> myStack;
-		while (ss.good())
-		{
+		while (ss.good()) {
 			ss >> str;
-			if (str.size() == 0)
-			{
+			if (str.size() == 0) {
 				continue;
 			}
-			if (str.size() == 1 && isOperator(str[0]))
-			{
+			if (str.size() == 1 && isOperator(str[0])) {
 				char op = str[0];
 				Bigex operand1 = myStack.top();
 				myStack.pop();
 				Bigex operand2 = myStack.top();
 				myStack.pop();
 
-				if (op == '+')
-				{
+				if (op == '+') {
 					myStack.push(operand1 + operand2);
 				}
-				else if (op == '-')
-				{
+				else if (op == '-') {
 					// myStack.push(operand1 - operand2);
 				}
-				else if (op == '*')
-				{
+				else if (op == '*') {
 					// myStack.push(operand1 * operand2);
 				}
 			}
-			else
-			{
+			else {
 				myStack.push(Bigex(str, digitsPerNode));
 			}
         }
@@ -507,19 +437,15 @@ class infixToPostfix
     }
 };
 
-bool infixToPostfix::isOperator(const char c)
-{
-
+bool infixToPostfix::isOperator(const char c) {
     if ((c == '+') || (c == '-') || (c == '*') || (c == '/'))
         return true;
     else
         return false;
 }
 
-int infixToPostfix::precedence(const char op1, const char op2)
-{
+int infixToPostfix::precedence(const char op1, const char op2) {
     int pre1 = 0, pre2 = 0; //declare variables to compare the precedence
-
     if ((op1 == '^') || (op2 == '^'))
         cout << "Exponentiation was not considered for precedence. \n\tThe program may result in abnormal output of Postfix exp.";
     if ((op1 == '+') || (op1 == '-')) //low precedence
@@ -541,58 +467,44 @@ int infixToPostfix::precedence(const char op1, const char op2)
 }
 
 //     conversion of infix arithmetic exp into postfix exp
-void infixToPostfix::showPostfix()
-{
+void infixToPostfix::showPostfix() {
     stackType<char> myStack; //A Stack of characters
     string pfx = "";
     infix.append(")"); //append a right parenthesis ')' to the end of infix
     myStack.push('('); //push the left parenthesis onto the stack
     unsigned int i = 0;
     if((infix[i]) == '-')
-    {
         infix[i] = '~';
-    }
-    do //loop through the infix array
-    {
-        if (isOperator(infix[i]) && infix[i + 1] == '-')
-        {
+    do //loop through the infix array {
+        if (isOperator(infix[i]) && infix[i + 1] == '-') {
             infix[i + 1] = '~';
         }
-        else if (isOperator(infix[i]) && infix[i + 1] == '+')
-        {
+        else if (isOperator(infix[i]) && infix[i + 1] == '+') {
             infix.erase(infix.begin() + i + 1);
         }
-        else if (isOperator(infix[i])) //If the operator is an element in the string
-        {
+        else if (isOperator(infix[i])) //If the operator is an element in the string {
             pfx += ' ';
-            if (!myStack.isEmptyStack() && isOperator(myStack.top())) //when the top of the stack is an operator
-            {
-                if (precedence(infix[i], myStack.top()) == 0) //find the precedence of the operator
-                {
+            if (!myStack.isEmptyStack() && isOperator(myStack.top())) //when the top of the stack is an operator {
+                if (precedence(infix[i], myStack.top()) == 0) //find the precedence of the operator {
                     pfx = pfx + myStack.top(); //place the operator in postfix
                     myStack.pop();             //Pop the stack
                 }
-                else if (precedence(infix[i], myStack.top()) == 1)
-                {
+                else if (precedence(infix[i], myStack.top()) == 1) {
                     myStack.push(infix[i]); //push into stack the infix elem when higher precedence
                     i++;
                 }
-                else
-                {
+                else {
                     pfx = pfx + myStack.top(); //place the element in postfix & pop the stack
                     myStack.pop();
                 }
             }
-            else
-            {
+            else {
                 myStack.push(infix[i]); //push onto stack when top of stack  is not an operator
                 i++;
             }
         }
-        else if (infix[i] == ')')
-        {
-            while (!myStack.isEmptyStack() && myStack.top() != '(') //get the elements from the stack
-            {
+        else if (infix[i] == ')') {
+            while (!myStack.isEmptyStack() && myStack.top() != '(') //get the elements from the stack {
                 pfx += ' ';            
                 pfx = pfx + myStack.top();
                 myStack.pop();
@@ -600,29 +512,23 @@ void infixToPostfix::showPostfix()
             myStack.pop(); //pop the '(' from the stack
             i++;
         }
-        else if (infix[i] == '(')
-        {
+        else if (infix[i] == '(') {
             myStack.push(infix[i]); //neither operator nor operand push onto stack
-            if (infix[i + 1] == '-')
-            {
+            if (infix[i + 1] == '-') {
                 infix.erase(infix.begin() + i + 1);
                 pfx += '-';
             }
-            else if (infix[i + 1] == '+')
-            {
+            else if (infix[i + 1] == '+') {
                 infix.erase(infix.begin() + i + 1);
                 pfx += '+';
             }
             i++;
         } //end if
-        else
-        {
-            if (infix[i] == '~')
-            {
+        else {
+            if (infix[i] == '~') {
                 pfx += '-';
             }
-            else
-            {
+            else {
                 pfx = pfx + infix[i]; //In case of operands, copy the operands  from infix to postfix
             }
             i++;
@@ -632,36 +538,29 @@ void infixToPostfix::showPostfix()
     postfix = pfx;
 }
 
-// void print(Bigex *a, int size, ostream & out)
-// {
+// void print(Bigex *a, int size, ostream & out) {
 //     a[0].print(out);
-//     for (int i = 1; i < size; i++)
-//     {
+//     for (int i = 1; i < size; i++) {
 //         out << endl;
 //         a[i].print(out);
 //     }
 // }
 
-int count_lines(string &filename, int digitsPerNode)
-{
+int count_lines(string &filename, int digitsPerNode) {
     std::ifstream ifs(filename.c_str());
 
     int counter = 0;
-    while (!ifs.eof())
-    {
+    while (!ifs.eof()) {
         string line;
         ifs >> line;
         counter++;
     }
-
     return counter;
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int digitsPerNode = 3;
-    // if (argc < 2)
-    // {
+    // if (argc < 2) {
     //     //std::cerr("Usage: infinitearithmetic \"input=xyz.txt;digitsPerNode=<number>\"\n");
     //     return 1;
     // }
@@ -675,14 +574,12 @@ int main(int argc, char *argv[])
 
     ifstream infile;
     infile.open("10.txt", ios::in);
-    if (!infile)
-    {
+    if (!infile) {
         cout << "Cannot open input file. Program terminates!!!" << endl;
         return 1;
     }
     getline(infile, infix); //reading first line
-    while (infile)
-    {
+    while (infile) {
         InfixExp.getInfix(infix);
         InfixExp.showInfix();
         //InfixExp.convert_sign();
